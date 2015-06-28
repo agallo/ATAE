888bf787d4136470822f1c2bef5c2a5ff3c2ce48
agallo <agallo@gwu.edu>
2015-06-27 23:46:34 -0400

    added command line arg option for ASN

--- ATAE.py
+++ ATAE.py
@@ -5,9 +5,26 @@ __author__ = 'agallo'
 # use peeringDB 2.0 API to see if a given ASN lists itself on the Equinix-Ashburn IX
 
 import urllib, json
+from argparse import ArgumentParser
+
+# setup some command line arguments
+
+parser = ArgumentParser(description="use peeringDB 2.0 API to see if a given ASN lists itself"
+                                    " on the Equinix-Ashburn IX")
+
+parser.add_argument('ASN', metavar='ASN', type=int)
+
+args = parser.parse_args()
+
+ASN = args.ASN
 
 
-baseurl = "https://beta.peeringdb.com/api/asn/6079"
+baseurl = "https://beta.peeringdb.com/api/asn/" + str(ASN)
+
+print baseurl
+
+'''
+
 
 raw = urllib.urlopen(baseurl);
 jresponse = json.load(raw)
@@ -18,3 +35,4 @@ for index, facility in enumerate(d['facility'] for d in faclist):
     if facility == 1:
         print "YAY! They're at Equinix-Ashburn"
     print index, facility
+'''
